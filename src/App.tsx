b96e1165a56ae1de12028d67a80646c2cf1f1cbc
[dyad] <git@dyad.sh>
2025-11-13 10:50:14 +0200

    [dyad] Set dashboard as homepage - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import GroupsPage from "./pages/Groups";
 import DashboardPage from "./pages/Dashboard";
@@ -21,7 +20,7 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<DashboardPage />} />
           <Route path="/groups" element={<GroupsPage />} />
           <Route path="/dashboard" element={<DashboardPage />} />
           <Route path="/messages" element={<MessagesPage />} />
