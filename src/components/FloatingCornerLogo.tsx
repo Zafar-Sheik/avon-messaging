459afef8f0575375f20b8f24ac6aac4af34b0a55
[dyad] <glasscharles369@gmail.com>
2025-11-15 14:59:01 +0200

    [dyad] Shift background logo to the right - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/FloatingCornerLogo.tsx
+++ src/components/FloatingCornerLogo.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const FloatingCornerLogo: React.FC = () => {
   return (
     <div className="fixed inset-0 z-0 pointer-events-none flex items-center justify-center">
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center translate-x-12 md:translate-x-24">
         <img
           src="/images/contact-online-solutions-logo.png"
           alt="Contact Online Solutions"
