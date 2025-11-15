8da4cd2b038f2cff3d8c0202c68ca34a7c1031ac
[dyad] <glasscharles369@gmail.com>
2025-11-15 14:58:18 +0200

    [dyad] Replace background image with new logo - wrote 2 file(s) + extra files edited outside of Dyad

--- src/components/FloatingCornerLogo.tsx
+++ src/components/FloatingCornerLogo.tsx
@@ -7,7 +7,7 @@ const FloatingCornerLogo: React.FC = () => {
     <div className="fixed inset-0 z-0 pointer-events-none flex items-center justify-center">
       <div className="flex items-center justify-center">
         <img
-          src="/images/output-onlinepngtools.png"
+          src="/images/contact-online-solutions-logo.png"
           alt="Contact Online Solutions"
           className="w-[85vw] md:w-[60vw] h-auto opacity-20 select-none"
         />
