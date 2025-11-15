95c40b8f94ee85011410b2b8f4d3d0a8b38ca8b0
[dyad] <glasscharles369@gmail.com>
2025-11-15 14:56:38 +0200

    [dyad] Center and fade background image - wrote 1 file(s) + extra files edited outside of Dyad

--- src/components/FloatingCornerLogo.tsx
+++ src/components/FloatingCornerLogo.tsx
@@ -4,12 +4,12 @@ import React from "react";
 
 const FloatingCornerLogo: React.FC = () => {
   return (
-    <div className="fixed bottom-4 right-4 z-50 pointer-events-none">
-      <div className="flex items-end">
+    <div className="fixed inset-0 z-0 pointer-events-none flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <img
           src="/images/output-onlinepngtools.png"
           alt="Contact Online Solutions"
-          className="h-48 md:h-64 w-auto drop-shadow-2xl select-none"
+          className="w-[85vw] md:w-[60vw] h-auto opacity-20 select-none"
         />
       </div>
     </div>
