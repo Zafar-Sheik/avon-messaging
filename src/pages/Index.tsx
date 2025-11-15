03cde11fc7f1e6b00ff905e51b7dc9daefffb081
[dyad] <glasscharles369@gmail.com>
2025-11-15 11:31:45 +0200

    [dyad] Check which dashboard page to edit - wrote 1 file(s) + extra files edited outside of Dyad

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,7 +1,5 @@
 "use client";
 import DashboardStats from "@/components/DashboardStats";
-import Reminders from "@/components/Reminders";
-import ExcelDashboard from "@/components/ExcelDashboard";
 import ThemeToggle from "@/components/ThemeToggle";
 
 const Index = () => {
@@ -9,13 +7,6 @@ const Index = () => {
     <div className="min-h-screen p-4 bg-background">
       <div className="p-4">
         <DashboardStats />
-      </div>
-      <div className="p-4">
-        <Reminders />
-      </div>
-      <div className="p-4">
-        {" "}
-        <ExcelDashboard />
       </div>
     </div>
   );
