b3a8ece1937809ff333f1f7bb8fe2c4a4f80bd77
[dyad] <glasscharles369@gmail.com>
2025-11-23 14:43:35 +0200

    [dyad] Removed Quick Access from the overview page - wrote 1 file(s)

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import DashboardStats from "@/components/DashboardStats";
-import QuickLinks from "@/components/QuickLinks";
 import ThemeToggle from "@/components/ThemeToggle";
 
 const Index = () => {
@@ -8,9 +7,6 @@ const Index = () => {
     <div className="min-h-screen p-4 bg-background">
       <div className="p-4">
         <DashboardStats />
-      </div>
-      <div className="p-4">
-        <QuickLinks />
       </div>
     </div>
   );
